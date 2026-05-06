f6c8439116fc8ed7ede8b295ae8a2fe1321aa5a3
Joao Brito <joao@example.com>
2026-05-06 21:17:13 +0100

    Step 2: Cache structure and multi-threading protections

---
+++ go.mod
@@ -0,0 +1,3 @@
+module delivery
+
+go 1.22.2
--- src/cache.go
+++ src/cache.go
@@ -2,14 +2,44 @@ package cache
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
+type cacheEntry struct {
+	data      []byte
+	timestamp time.Time
+	ttl       time.Duration
+}
+type URL string
+
+type loadingCacheEntry struct {
+	wg      sync.WaitGroup
+	waiters atomic.Int64
+
+	err error
+}
+
 type Cache struct {
+	defaultTTL time.Duration
+
+	// To be used for clean-up of expired entries
+	mutex sync.RWMutex
+
+	entries map[URL]*cacheEntry
+
+	// Last one waiting cleans up the loadingCache entry
+	loadingCacheEntries map[URL]*loadingCacheEntry
+	loadingCacheMutex   sync.RWMutex
 }
 
 func NewCache(defaultTTL time.Duration) *Cache {
-	return &Cache{}
+	return &Cache{
+		defaultTTL:          defaultTTL,
+		entries:             make(map[URL]*cacheEntry),
+		loadingCacheEntries: make(map[URL]*loadingCacheEntry),
+	}
 }
 
 func (c *Cache) Fetch(ctx context.Context, url string, ttlOverride ...time.Duration) ([]byte, error) {
