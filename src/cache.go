2f11d6785b5128b4b316f71ebf4a17316c64aa7f
Joao Brito <joao@example.com>
2026-05-06 22:03:26 +0100

    Step 4: Logging of cache events, implementation of Stas and implementation of TestCaseFetch

--- src/cache.go
+++ src/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"sync"
@@ -20,9 +21,15 @@ type loadingCacheEntry struct {
 	wg      sync.WaitGroup
 	waiters atomic.Int64
 
-	// Unfortunatelly we need to store data because when evaluating a loadingCache cache hit we only have a lock towards the loadingCache structure
+	// Unfortunatelly we need to store data because when evaluating a loading cache hit we only have a lock towards the loadingCache structure
+	// also pretty easily ttl can expire. the waiters get always the instant cache
 	data []byte
 	err  error
+}
+
+type statistics struct {
+	hits   int
+	misses int
 }
 
 type Cache struct {
@@ -33,17 +40,11 @@ type Cache struct {
 
 	entries map[URL]*cacheEntry
 
-	// Last one waiting cleans up the loadingCache entry
+	// Last one waiting cleans up the loading cache entry
 	loadingCacheEntries map[URL]*loadingCacheEntry
 	loadingCacheMutex   sync.RWMutex
-}
 
-func NewCache(defaultTTL time.Duration) *Cache {
-	return &Cache{
-		defaultTTL:          defaultTTL,
-		entries:             make(map[URL]*cacheEntry),
-		loadingCacheEntries: make(map[URL]*loadingCacheEntry),
-	}
+	stats statistics
 }
 
 func (c *Cache) cleanUpExpiredEntries() {
@@ -55,15 +56,6 @@ func (c *Cache) cleanUpExpiredEntries() {
 		if now.Sub(entry.timestamp) > entry.ttl {
 			delete(c.entries, url)
 		}
-	}
-}
-
-func (c *Cache) cacheUpdateTTL(url URL, ttl time.Duration) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	if entry, exists := c.entries[url]; exists {
-		entry.ttl = ttl
 	}
 }
 
@@ -85,7 +77,7 @@ func httpRequest(ctx context.Context, url string, method ...string) ([]byte, err
 	return io.ReadAll(resp.Body)
 }
 
-func (c *Cache) poploadingCacheHit(url URL, loadingCacheEntry *loadingCacheEntry) ([]byte, error) {
+func (c *Cache) popLoadingCacheHit(url URL, loadingCacheEntry *loadingCacheEntry) ([]byte, error) {
 	c.loadingCacheMutex.Lock()
 	defer c.loadingCacheMutex.Unlock()
 
@@ -96,27 +88,34 @@ func (c *Cache) poploadingCacheHit(url URL, loadingCacheEntry *loadingCacheEntry
 		data = loadingCacheEntry.data
 	}
 
-	// Last consumer cleans up the loading cache entry
-	if loadingCacheEntry.waiters.Load() == 0 {
+	// Last one cleans up the loading cache entry
+	if loadingCacheEntry.waiters.Add(-1) == 0 {
 		delete(c.loadingCacheEntries, url)
 	}
 
 	return data, err
 }
 
+func NewCache(defaultTTL time.Duration) *Cache {
+	return &Cache{
+		defaultTTL:          defaultTTL,
+		entries:             make(map[URL]*cacheEntry),
+		loadingCacheEntries: make(map[URL]*loadingCacheEntry),
+	}
+}
+
 func (c *Cache) Fetch(ctx context.Context, url string, ttlOverride ...time.Duration) ([]byte, error) {
 	ttl := c.defaultTTL
 
-	// override TTL if provided to already existing cache entries
-	// Since it's a Map in theory there is not much performance impact
+	// todo: really time consuming per request. Create a housekeeping routine to free-up memory
+	// This will only run every something requests. when we implement stats. No housekeeping!
+	// The reason is because we could have some kind of dos attack and we want to make sure we free up memory as soon as possible. We could also have a max cache size and free up the least recently used entries.
+	c.cleanUpExpiredEntries()
+
+	// override default ttl if provided
 	if len(ttlOverride) > 0 {
-		c.cacheUpdateTTL(URL(url), ttlOverride[0])
 		ttl = ttlOverride[0]
 	}
-
-	// It's possible to force a fetch if ttlOverride to 0
-	// todo: really time consuming per request. Create a housekeeping routine to free-up memory
-	c.cleanUpExpiredEntries()
 
 	// From this moment onwards the data either exists on valid cache or we need to fetch it
 	// Regardless of what we do, we need to be exclusive
@@ -124,7 +123,10 @@ func (c *Cache) Fetch(ctx context.Context, url string, ttlOverride ...time.Durat
 
 	// Check if cache entry exists and is valid
 	entry, exists := c.entries[URL(url)]
-	if exists && time.Since(entry.timestamp) <= entry.ttl {
+	if exists && time.Since(entry.timestamp) < entry.ttl {
+		c.stats.hits += 1
+		fmt.Printf("CACHE HIT %d\t: url=%s, elapsed=%v, ttl=%v\n", c.stats.hits, url, time.Since(entry.timestamp), entry.ttl)
+
 		c.mutex.Unlock()
 		return entry.data, nil
 	}
@@ -136,49 +138,62 @@ func (c *Cache) Fetch(ctx context.Context, url string, ttlOverride ...time.Durat
 
 	c.loadingCacheMutex.Lock()
 
-	// verify if the cache is loaded by another fetch. If so, we wait and use the loading cache
+	// Check if data exists in loading cache
 	if loadingCacheEntry, loadingCacheExists := c.loadingCacheEntries[URL(url)]; loadingCacheExists {
+		c.stats.hits += 1
+		fmt.Printf("CACHE HIT(LOAD) %d\t: url=%s, elapsed=%v, ttl=%v\n", c.stats.hits, url, time.Since(entry.timestamp), entry.ttl)
+
 		loadingCacheEntry.waiters.Add(1)
 		c.loadingCacheMutex.Unlock()
 		c.mutex.Unlock()
 
 		loadingCacheEntry.wg.Wait()
-		return c.poploadingCacheHit(URL(url), loadingCacheEntry)
+		return c.popLoadingCacheHit(URL(url), loadingCacheEntry)
 	}
 
 	// Lets create the loading cache entry and release the locks
 	loadingCacheEntry := &loadingCacheEntry{}
-	loadingCacheEntry.wg.Add(1)
 	c.loadingCacheEntries[URL(url)] = loadingCacheEntry
+
+	// Producer is also a waiter, to ensure we properly delete the loading cache entry
+	loadingCacheEntry.waiters.Add(1)
+	loadingCacheEntry.wg.Add(1)
 	c.loadingCacheMutex.Unlock()
 
 	c.mutex.Unlock()
 
-	// We now execute the http request without holding any lock
+	// Need to fetch the data as in loading cache
 	data, err := httpRequest(ctx, url)
 
-	// Store in loadingCache
+	// Store in loading cache
 	c.loadingCacheMutex.Lock()
 	c.loadingCacheEntries[URL(url)].err = err
 	c.loadingCacheEntries[URL(url)].data = data
 	c.loadingCacheMutex.Unlock()
 
-	// Store in cache entry if no error
+	// Store in cache if no error
 	if err == nil {
 		c.mutex.Lock()
 		c.entries[URL(url)] = &cacheEntry{
-			data:      data,
+			data:      data, // reference copy. GC manages it
 			timestamp: time.Now(),
 			ttl:       ttl,
 		}
+
+		c.stats.misses += 1
+		fmt.Printf("CACHE MISS %d\t: url=%s, elapsed=%v, ttl=%v\n", c.stats.misses, url, time.Since(c.entries[URL(url)].timestamp), c.entries[URL(url)].ttl)
 		c.mutex.Unlock()
 	}
 
-	// Lets make sure the producer also pops the loading Cache
-	// to ensure he also frees up the memory. This is a edge case in case there are any consumers waiting for the loading cache hit, but the producer also needs to pop the loading cache hit to ensure he also frees up the memory
-	return c.poploadingCacheHit(URL(url), loadingCacheEntry)
+	// Lets release all the waiters:
+	loadingCacheEntry.wg.Done()
+
+	// Lets make sure the producer also pops the loading cache hit to ensure he also frees up the memory
+	return c.popLoadingCacheHit(URL(url), loadingCacheEntry)
 }
 
 func (c *Cache) Stats() (hits int, misses int, entries int) {
-	return 0, 0, 0
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	return c.stats.hits, c.stats.misses, len(c.entries)
 }
--- src/cache_test.go
+++ src/cache_test.go
@@ -1,11 +1,118 @@
 package cache
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"sync/atomic"
 	"testing"
+	"time"
 )
 
+func setupTest(t *testing.T, defaultTTL time.Duration, httpServerCount *atomic.Int64) (*Cache, *httptest.Server, string) {
+	// 1Megabyte random payload
+	randomBytes := make([]byte, 512*1024)
+	rand.Read(randomBytes)
+	expected_payload := hex.EncodeToString(randomBytes)
+
+	listener, _ := net.Listen("tcp", "127.0.0.1:0")
+	server := &httptest.Server{
+		Listener: listener,
+		Config: &http.Server{
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte(expected_payload))
+				httpServerCount.Add(1)
+			}),
+		},
+	}
+	server.Start()
+
+	c := NewCache(defaultTTL)
+	return c, server, expected_payload
+}
+
+func assertFetch(t *testing.T, c *Cache, url string, expected string, ttlOverride ...time.Duration) {
+	ttl := c.defaultTTL
+	if len(ttlOverride) > 0 {
+		ttl = ttlOverride[0]
+	}
+	data, err := c.Fetch(context.Background(), url, ttl)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if string(data) != expected {
+		t.Fatalf("expected %q, got %q", expected, string(data))
+	}
+
+	// lets give a small 1ms delay to make sure the next entry is created with different timestamp
+	// if cpu is too fast sometimes timestamps are the same
+	time.Sleep(1 * time.Microsecond)
+}
+
+func assertServerCount(t *testing.T, httpServerCount atomic.Int64, expected int64) {
+	if httpServerCount.Load() != expected {
+		t.Fatalf("expected %d server calls, got %d", expected, httpServerCount.Load())
+	}
+}
+
+func assertStats(t *testing.T, c *Cache, expectedHits int, expectedMisses int, expectedEntries int) {
+	hits, misses, entries := c.Stats()
+	if hits != expectedHits {
+		t.Fatalf("expected %d hits, got %d , assertStats(%d, %d, %d)", expectedHits, hits, expectedHits, expectedMisses, expectedEntries)
+	}
+	if misses != expectedMisses {
+		t.Fatalf("expected %d misses, got %d , assertStats(%d, %d, %d)", expectedMisses, misses, expectedHits, expectedMisses, expectedEntries)
+	}
+	if entries != expectedEntries {
+		t.Fatalf("expected %d entries, got %d , assertStats(%d, %d, %d)", expectedEntries, entries, expectedHits, expectedMisses, expectedEntries)
+	}
+}
+
 func TestCacheFetch(t *testing.T) {
-	// TODO: simulate an HTTP server and test caching behavior
+	var httpServerCount atomic.Int64
+	c, server, expected_payload := setupTest(t, 5*time.Minute, &httpServerCount)
+	defer server.Close()
+
+	assertFetch(t, c, server.URL, expected_payload, 0)
+	assertServerCount(t, httpServerCount, 1)
+	assertStats(t, c, 0, 1, 1) // MISS
+
+	assertFetch(t, c, server.URL, expected_payload, 0)
+	assertServerCount(t, httpServerCount, 2)
+	assertStats(t, c, 0, 2, 1) // MISS
+
+	assertFetch(t, c, server.URL, expected_payload)
+	assertServerCount(t, httpServerCount, 3)
+	assertStats(t, c, 0, 3, 1) // MISS
+
+	assertFetch(t, c, server.URL, expected_payload)
+	assertServerCount(t, httpServerCount, 3)
+	assertStats(t, c, 1, 3, 1) // HIT
+
+	assertFetch(t, c, server.URL, expected_payload)
+	assertServerCount(t, httpServerCount, 3)
+	assertStats(t, c, 2, 3, 1) // HIT
+
+	var httpServer2Count atomic.Int64
+	_, server2, expected_payload2 := setupTest(t, 5*time.Minute, &httpServer2Count)
+
+	assertFetch(t, c, server2.URL, expected_payload2, 0)
+	assertServerCount(t, httpServerCount, 3)
+	assertServerCount(t, httpServer2Count, 1)
+	assertStats(t, c, 2, 4, 2) // MISS
+
+	assertFetch(t, c, server2.URL, expected_payload2)
+	assertServerCount(t, httpServerCount, 3)
+	assertServerCount(t, httpServer2Count, 2)
+	assertStats(t, c, 2, 5, 2) // MISS
+
+	assertFetch(t, c, server2.URL, expected_payload2, 0)
+	assertServerCount(t, httpServerCount, 3)
+	assertServerCount(t, httpServer2Count, 2)
+	assertStats(t, c, 3, 5, 2) // HIT
 }
 
 func TestCacheConcurrency(t *testing.T) {
